58fc6fd58b56a5219a3f1cc467dc949e96575e70
ZHANGHD715 <hdzhang@thoughtworks.com>
2016-09-22 23:33:55 +0800

    refactor the stickey header function with throttle

--- themes/tootopia/assets/js/stickeyHeader.js
+++ themes/tootopia/assets/js/stickeyHeader.js
@@ -6,25 +6,53 @@
 	var $header = $('#masthead');
 	var $main = $('#main');
 	var fixHeaderClass = 'fixedHeader';
-	var topLengthToStartFixed = 450;
+	var TOP_LENGTH = 350;
 
-	$window.on(' scroll ', function(){
-
+	var updateHeader = function () {
 		var scrollTop = $window.scrollTop();
-		
-		if ( topLengthToStartFixed > 350 && !$header.hasClass(fixHeaderClass) ) {
+			
+		if ( scrollTop > TOP_LENGTH && !$header.hasClass(fixHeaderClass) ) {
 			$main.css('margin-top', '100px');
 			$header.hide();
-			$header.fadeIn(1500);
+			$header.fadeIn(1000);
 			$header.addClass(fixHeaderClass);
 
 		} 
 
-		if ( topLengthToStartFixed <= 350 && $header.hasClass(fixHeaderClass) ) {
+		if ( scrollTop <= TOP_LENGTH && $header.hasClass(fixHeaderClass) ) {
 			$main.css('margin-top', '0');
 			$header.removeClass('fixedHeader');
 		}
+	}
 
-	});
+	// 使用节流函数
+	var throttle = function() {
+		var firsttime = true,
+			fixedHeaderTimer;
+
+		return function () {
+
+			if (firsttime) {
+				updateHeader();
+				firsttime = false;
+				return;
+			}
+
+			if (fixedHeaderTimer) {
+				return false;
+			}
+
+			fixedHeaderTimer = setTimeout(function(){
+
+				clearTimeout(fixedHeaderTimer);
+				fixedHeaderTimer = null;
+				updateHeader();
+
+			}, 300);
+
+		}
+	}
+	
+	$window.on(' scroll ', throttle());
 
 }(jQuery);
