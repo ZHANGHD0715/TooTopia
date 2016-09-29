12c117f9669cbbf73d92f675e7ca485ecba55cd4
ZHANGHD715 <hdzhang@thoughtworks.com>
2016-09-29 18:09:51 +0800

    modified tag page

--- themes/tootopia/tag.php
+++ themes/tootopia/tag.php
@@ -17,7 +17,7 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-6">
-                <h1 class="archive-title"><?php printf( __( 'Tag Archives: %s', 'retouch' ), '<span>' . single_tag_title( '', false ) . '</span>' ); ?></h1>
+                <h1 class="archive-title"><?php printf( __( '热门话题: %s', 'retouch' ), '<span>' . single_tag_title( '', false ) . '</span>' ); ?></h1>
                 <?php if ( tag_description() ) : // Show an optional tag description ?>
                 <div class="archive-meta"><?php echo tag_description(); ?></div>
                 <?php endif; ?>
@@ -33,34 +33,32 @@
     <!-- /.container -->
 </header><!-- .archive-header -->
 <div id="main" class="wrapper">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8">
-                <section id="primary" class="site-content">
-                    <div id="content" role="main">
-                        <?php if ( have_posts() ) : ?>
+    <section id="primary" class="site-content">
+        <div id="content" role="main">
+            <div class="container">
+                <div id="content" role="main">
+                    <div class="row">
 
-                        <?php
-                            /* Start the Loop */
-                            while ( have_posts() ) : the_post();
-                                /* Include the post format-specific template for the content. If you want to
-                                 * this in a child theme then include a file called called content-___.php
-                                 * (where ___ is the post format) and that will be used instead.
-                                 */
-                                get_template_part( 'content', get_post_format() );
-                            endwhile;
-                            retouch_content_nav( 'nav-below' );
-                        ?>
-                        <?php else : ?>
-                        <?php get_template_part( 'content', 'none' ); ?>
-                        <?php endif; ?>
-                    </div><!-- #content -->
-                </section><!-- #primary -->
-            </div>
-            <div class="col-md-4">
-                <?php get_sidebar(); ?>
+                    <?php if ( have_posts() ) : ?>
+
+                        <?php while ( have_posts() ) : the_post(); ?>
+                            
+                            <div class="col-xs-12 col-md-4 col-lg-3 article-wrap isotope-item" >
+                            <?php get_template_part( 'template-parts/content', 'teaser' ); ?>
+                            </div>
+
+                        <?php endwhile; ?>
+
+                        <?php retouch_content_nav( 'nav-below' ); ?>
+
+                    <?php else : ?>
+                    <?php get_template_part( 'content', 'none' ); ?>
+                    <?php endif; ?>
+
+                    </div>
+                </div><!-- #content -->
             </div>
         </div>
-    </div>
+    </section><!-- #primary -->
 </div><!-- #main .wrapper -->
 <?php get_footer(); ?>
